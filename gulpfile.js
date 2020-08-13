113b7d945e86e130a00c8e2b00ec577e81e9636c
Virola <wetsky90@126.com>
2020-08-13 15:09:22 +0800

    gulp更新之后不能用了，干脆放弃压缩先发布吧

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,33 @@
-var gulp = require('gulp');
-var minifycss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
+const { src, dest, parallel } = require('gulp')
+const minifyCSS = require('gulp-csso')
+const htmlmin = require('gulp-htmlmin')
 
-// 获取 gulp-imagemin 模块
-var imagemin = require('gulp-imagemin')
+function html() {
+  return src(['public/**/*.html', 'public/*.html'])
+    .pipe(
+      htmlmin({
+        removeComments: true, //清除HTML注释
+        collapseWhitespace: true, //压缩HTML
+        collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input checked />
+        removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+        minifyJS: true, //压缩页面JS
+        minifyCSS: true //压缩页面CSS
+      })
+    )
+    .pipe(dest('build/'))
+}
 
-// 压缩 public 目录 css
-gulp.task('minify-css', function () {
-    return gulp.src('./public/**/*.css')
-        .pipe(minifycss())
-        .pipe(gulp.dest('./public'));
-});
-// 压缩 public 目录 html
-gulp.task('minify-html', function () {
-    return gulp.src('./public/**/*.html')
-        .pipe(htmlclean())
-        .pipe(htmlmin({
-            removeComments: true,  //清除HTML注释
-            collapseWhitespace: true,  //压缩HTML
-            collapseBooleanAttributes: true,  //省略布尔属性的值 <input checked="true"/> ==> <input checked />
-            removeEmptyAttributes: true,  //删除所有空格作属性值 <input id="" /> ==> <input />
-            removeScriptTypeAttributes: true,  //删除<script>的type="text/javascript"
-            removeStyleLinkTypeAttributes: true,  //删除<style>和<link>的type="text/css"
-            minifyJS: true,  //压缩页面JS
-            minifyCSS: true  //压缩页面CSS
-        }))
-        .on('error', function (err) {
-            console.log('html Error!', err.message);
-            this.end();
-        })
-        .pipe(gulp.dest('./public'))
-});
-// 压缩 public/js 目录 js
-gulp.task('minify-js', function () {
-    return gulp.src('./public/**/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./public'));
-});
+function css() {
+  return src('public/css/*.css').pipe(minifyCSS()).pipe(dest('build/'))
+}
 
+function js() {
+  return src('public/js/*.js', { sourcemaps: true }).pipe(dest('build/', { sourcemaps: true }))
+}
 
-// 压缩图片任务
-// 在命令行输入 gulp images 启动此任务
-gulp.task('images', function () {
-    // 1. 找到图片
-    gulp.src('./album/*.*')
-        // 2. 压缩图片
-        .pipe(imagemin({
-            progressive: true
-        }))
-        // 3. 另存图片
-        .pipe(gulp.dest('dist/images'))
-});
-
-// 执行 gulp 命令时执行的任务
-gulp.task('build', [
-    'minify-html', 'minify-css', 'minify-js', 'images',
-]);
+exports.js = js
+exports.css = css
+exports.html = html
+exports.default = parallel(html, css, js)
